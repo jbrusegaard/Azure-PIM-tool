cb743d58df8e38efccb9a22d85be3b19a52b43b8
dan <11095114+poseidonphp@users.noreply.github.com>
2025-08-18 10:28:33 -0500

    support for multiple roles in one request

--- src/activate.go
+++ src/activate.go
@@ -1,18 +1,18 @@
 package src
 
 import (
+	"encoding/json"
+
 	"app/azuClient"
 	"app/constants"
-	"encoding/json"
-	"fmt"
 
 	"github.com/charmbracelet/log"
 )
 
 type ActivationOptions struct {
-	Reason    string
-	Duration  int    // Duration in hours
-	GroupName string // Filter criteria for activation
+	Reason     string
+	Duration   int      // Duration in hours
+	GroupNames []string // Filter criteria for activation
 }
 
 func ActivatePim(opts ActivationOptions) {
@@ -21,38 +21,45 @@ func ActivatePim(opts ActivationOptions) {
 		AzurePimToken: appSettings.Session.AZPimToken,
 	}
 	eligibleRoleMap, err := azureClient.GetEligibleRoles(constants.AzurePimGroupApiUrlRoleAssignments)
+	if err != nil {
+		log.Error("Error fetching eligible roles:", err)
+		return
+	}
 
 	if len(eligibleRoleMap) == 0 {
-		fmt.Println("No eligible roles found.")
+		log.Warn("No eligible roles found.")
 	}
-	roleToActivate, found := eligibleRoleMap[opts.GroupName]
+	for _, groupName := range opts.GroupNames {
+		roleToActivate, found := eligibleRoleMap[groupName]
 
-	if !found {
-		fmt.Println("No eligible group found with the specified name:", opts.GroupName)
-		return
-	}
-
-	log.Infof("Activating role: %s", roleToActivate.GetGroupName())
-	requestBody := azuClient.BuildPimRequestBody(
-		roleToActivate,
-		roleToActivate.Subject.Id,
-		opts.Reason,
-		opts.Duration,
-	)
-	resp, err := azureClient.Activate(constants.AzurePimGroupApiUrlRoleAssigmentRequests, requestBody)
-	if err != nil {
-		if resp != "" {
-			var errResp *azuClient.AzureGroupErrorResponse
-			unmarshErr := json.Unmarshal([]byte(resp), &errResp)
-			if unmarshErr != nil {
-				log.Error(err.Error())
-				return
-			}
-			log.Warn(errResp.Error.Message)
-		} else {
-			log.Error(err.Error())
+		if !found {
+			log.Warnf("No eligible group found with the specified name: %s", groupName)
+			continue
 		}
-		return
+
+		log.Infof("Activating role: %s", roleToActivate.GetGroupName())
+		requestBody := azuClient.BuildPimRequestBody(
+			roleToActivate,
+			roleToActivate.Subject.Id,
+			opts.Reason,
+			opts.Duration,
+		)
+		resp, err := azureClient.Activate(constants.AzurePimGroupApiUrlRoleAssigmentRequests, requestBody)
+		if err != nil {
+			if resp != "" {
+				var errResp *azuClient.AzureGroupErrorResponse
+				unmarshErr := json.Unmarshal([]byte(resp), &errResp)
+				if unmarshErr != nil {
+					log.Error(err.Error())
+					return
+				}
+				log.Warn(errResp.Error.Message)
+			} else {
+				log.Error(err.Error())
+			}
+			return
+		}
+		log.Infof("Successfully activated role: %s!", roleToActivate.GetGroupName())
 	}
-	log.Infof("Successfully activated role: %s!", roleToActivate.GetGroupName())
+
 }
